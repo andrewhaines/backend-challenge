442c077fdd1340bb906e60bc4402127ce3006ba0
Andrew Haines <andrew.m.haines@gmail.com>
2020-12-29 21:17:46 -0500

    Refactor and adjust friends search WIP

--- app/models/heading.rb
+++ app/models/heading.rb
@@ -2,17 +2,12 @@ class Heading < ApplicationRecord
   belongs_to :member
 
   def search_connections(searcher, expert)
-    connections_string = String.new
-    connection_ids = []
-    connection_ids << expert.id
-
-    friend_queue = []
-    searched_friends = []
+    connections_string = String.new(" -> #{expert.name}")
+    connection_ids = [expert.id]
 
     # End with the expert and work out
-    friend_queue << expert
-    searched_friends << expert
-    connections_string.concat(" -> #{expert.name}")
+    friend_queue = [expert]
+    searched_friends = [expert]
 
     while friend_queue.length > 0 do
       friend = friend_queue.shift # Cycle to the next member
@@ -26,21 +21,33 @@ class Heading < ApplicationRecord
     end
 
     # Remove any connections that are dead ends in the path
-    revised_ids = connection_ids.reverse.drop(1)
+    revised_ids = connection_ids.reverse
+    removed_ids = []
 
     revised_ids.each_cons(2) do |id, next_id|
-      member = Member.find(id)
+      unless removed_ids.include?(id) # Skip IDs we've already removed
+        member = Member.find(id)
 
-      # Test the friend relationship and remove the false positive
-      unless member.friends.where(id: next_id).any?
-        revised_ids.delete(next_id)
+        # Test the friend relationships and remove the false positives
+        if member.id == searcher.id || searcher.friends.where(id: id).any?
+          # this one's good, the next one might not be though..
+          unless member.friends.where(id: next_id).any?
+            removed_ids << next_id
+            revised_ids.delete(next_id)
+          end
+        elsif !member.friends.where(id: next_id).any?
+          removed_ids << id
+          revised_ids.delete(id)
+        end
       end
     end
 
+    # Remove the searcher ID since it's loaded in the UI; and the expert ID since it was added first
     revised_ids.delete(searcher.id)
+    revised_ids.delete(expert.id)
 
-    revised_ids.reverse.drop(1).each do |id|
-      # Make it readable
+    revised_ids.reverse.each do |id|
+      # Make the list readable
       connections_string.prepend(" -> #{Member.find(id).name}")
     end
 
